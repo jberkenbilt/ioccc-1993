0779ad0d1becd4d05ff29bb24122ecb00f1223ba
Jay Berkenbilt <ejb@ql.org>
1993-03-13 17:59:49 +0000

    Got rid of one more set of parentheses; added comment at the end
    RCS: qjbprog.c: 1.23

--- qjbprog.c
+++ qjbprog.c
@@ -3,7 +3,7 @@ char a[] = {0x69, 110, 118, 97, 108, 105, 0x64, 1-1, 0x6d, 111, 118,
 114, 111, 0x6d, 32, 115, 116, 97, 100-001, 107, 32, 37, 2*'2', 32,
 116, '%'+'%'+'%', 'w'-'W', 115, 0x74, 97, 3*'!', 107, 'q'-'Q', 37,
 10*'\n', 10, 0}; char *b = a + (1<<1<<1<<1); char *w; signed char x;
-int main(int d, char *e[]) { char *q = ((char *)(e + 1+1)); if ((!
+int main(int d, char *e[]) { char *q = (char *)(e + 1+1); if ((!
 e[0]) || (! e[1])) return puts(a), 1; else if (e[1<<1] && d != 1<<1)
 return puts(a), 1; else if (!e[1- -1]) { if (atoi(e[1]) < -0200 ||
 atoi(e[1]) > 0x7f || (x = atoi(e[1])) == 0) return puts(a), 1; if (x >
@@ -21,4 +21,4 @@ puts(w, w[- x - 1] = q[1]); break; default: q[0] += 1 + (q[1] & 1 ^
 q[1]^=1; main(d, e); q[1]^=1; q[0]-=1+1; } puts(w, w[q[0] - x] =
 q[1]); if (!(q[1] & 1)) { q[0]+=1+1; q[1]^=1; main(d, e); q[1]^=1;
 q[0]-=1+1; } q[0] += 1 + (q[1] & 1); main(d, e); q[0] -= 1 + (q[1] &
-1); } } } return 0; }
+1); } } } return 0; } /* The end */
