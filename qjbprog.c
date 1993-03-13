d53c7af5c612cae47b4bd8c068d83f4c6c8f06b2
Jay Berkenbilt <ejb@ql.org>
1993-03-13 16:38:57 +0000

    ready to obfuscate variable names
    RCS: qjbprog.c: 1.17

--- qjbprog.c
+++ qjbprog.c
@@ -6,54 +6,10 @@ char *m1 = "move ring %d from stack %d to stack %d\n";
 char *bits;
 signed char len;
 
-void move(char *args)
-{
-    if (args[3])
-    {
-	char c, t;
-	c = (args[1] + args[2] == 3) ? 3 : (args[1] + args[2] == 4) ? 2 : 1;
-	if (args[0] != 1) {
-	    (args[0])--; t = args[2]; args[2] = c; move(args); c = args[2]; args[2] = t; (args[0])++;
-	}
-	printf(m1, args[0], args[1], args[2]);
-	if (args[0] != 1) {
-	    (args[0])--; t = args[1]; args[1] = c; move(args); c = args[1]; args[1] = t; (args[0])++;
-	}
-    }
-    else
-    {
-	switch (- 1 + (-(args[0])) - 1)
-	{
-	  case 0:
-	    puts(bits, bits[- len - (1 + ((args[1] & 1) ^ 1))] = args[1]);
-	    puts(bits, bits[- len - (1 + (args[1] & 1))] = args[1]);
-	    break;
-	  case -1:
-	    puts(bits, bits[- len - 1] = args[1]);
-	    break;
-	  default:
-	    args[0] += 1 + ((args[1] & 1) ^ 1);
-	    move(args);
-	    args[0] -= 1 + ((args[1] & 1) ^ 1);
-	    if (args[1] & 1) {
-		(args[0])+=2; (args[1]) ^= 1; move(args); (args[1]) ^= 1; (args[0])-=2;
-	    }
-	    puts(bits, bits[args[0] - len] = args[1]);
-	    if (!(args[1] & 1)) {
-		(args[0])+=2; (args[1]) ^= 1; move(args); (args[1]) ^= 1; (args[0])-=2;
-	    }
-	    args[0] += 1 + (args[1] & 1);
-	    move(args);
-	    args[0] -= 1 + (args[1] & 1);
-	}
-    }
-}
-
-
 int main(int argc, char *argv[])
 {
     char *args = (char *)(argv + 2);
-
+    
     if ((! argv[0]) || (! argv[1]))
 	return printf(m0, argv[0]), 1;
     else if (argv[2] && (argc != 2))
@@ -83,7 +39,46 @@ int main(int argc, char *argv[])
 	return main(argc, argv);
     }
     else
-	move(args);
-
+    {
+	if (args[3])
+	{
+	    char c, t;
+	    c = (args[1] + args[2] == 3) ? 3 : (args[1] + args[2] == 4) ? 2 : 1;
+	    if (args[0] != 1) {
+		(args[0])--; t = args[2]; args[2] = c; main(argc, argv); c = args[2]; args[2] = t; (args[0])++;
+	    }
+	    printf(m1, args[0], args[1], args[2]);
+	    if (args[0] != 1) {
+		(args[0])--; t = args[1]; args[1] = c; main(argc, argv); c = args[1]; args[1] = t; (args[0])++;
+	    }
+	}
+	else
+	{
+	    switch (- 1 + (-(args[0])) - 1)
+	    {
+	      case 0:
+		puts(bits, bits[- len - (1 + ((args[1] & 1) ^ 1))] = args[1]);
+		puts(bits, bits[- len - (1 + (args[1] & 1))] = args[1]);
+		break;
+	      case -1:
+		puts(bits, bits[- len - 1] = args[1]);
+		break;
+	      default:
+		args[0] += 1 + ((args[1] & 1) ^ 1);
+		main(argc, argv);
+		args[0] -= 1 + ((args[1] & 1) ^ 1);
+		if (args[1] & 1) {
+		    (args[0])+=2; (args[1]) ^= 1; main(argc, argv); (args[1]) ^= 1; (args[0])-=2;
+		}
+		puts(bits, bits[args[0] - len] = args[1]);
+		if (!(args[1] & 1)) {
+		    (args[0])+=2; (args[1]) ^= 1; main(argc, argv); (args[1]) ^= 1; (args[0])-=2;
+		}
+		args[0] += 1 + (args[1] & 1);
+		main(argc, argv);
+		args[0] -= 1 + (args[1] & 1);
+	    }
+	}
+    }
     return 0;
 }
