b229f6bd28b0a9cf03ffde397d8b29481d2dd042
Jay Berkenbilt <ejb@ql.org>
1993-03-13 16:40:33 +0000

    ready to eliminate indirect access to argv[2]
    RCS: qjbprog.c: 1.18

--- qjbprog.c
+++ qjbprog.c
@@ -1,82 +1,82 @@
 extern void *malloc(int);
 
-char *m0 = "Usage: %s [+|-]n\n\tn != 0, -128 < n < 127\n";
-char *m1 = "move ring %d from stack %d to stack %d\n";
+char *v0 = "Usage: %s [+|-]n\n\tn != 0, -128 < n < 127\n";
+char *v1 = "move ring %d from stack %d to stack %d\n";
 
 char *bits;
 signed char len;
 
-int main(int argc, char *argv[])
+int main(int v2, char *v3[])
 {
-    char *args = (char *)(argv + 2);
+    char *v4 = (char *)(v3 + 2);
     
-    if ((! argv[0]) || (! argv[1]))
-	return printf(m0, argv[0]), 1;
-    else if (argv[2] && (argc != 2))
-	return printf(m0, argv[0]), 1;
-    else if (!argv[2])
+    if ((! v3[0]) || (! v3[1]))
+	return printf(v0, v3[0]), 1;
+    else if (v3[2] && (v2 != 2))
+	return printf(v0, v3[0]), 1;
+    else if (!v3[2])
     {
-	if ((atoi(argv[1]) < -128) || (atoi(argv[1]) > 127) ||
-	    ((len = atoi(argv[1])) == 0))
-	    return printf(m0, argv[0]), 1;
+	if ((atoi(v3[1]) < -128) || (atoi(v3[1]) > 127) ||
+	    ((len = atoi(v3[1])) == 0))
+	    return printf(v0, v3[0]), 1;
 	if (len > -len)
 	{
-	    args[0] = len;
-	    args[1] = 1;
-	    args[2] = 2;
-	    args[3] = 1;
+	    v4[0] = len;
+	    v4[1] = 1;
+	    v4[2] = 2;
+	    v4[3] = 1;
 	}
 	else if (len < 0)
 	{
 	    bits = malloc(-len);
 	    memset(bits, '1', -len);
 	    puts(bits);
-	    args[0] = len;
-	    args[1] = '0';
-	    args[2] = 0;
-	    args[3] = 0;
+	    v4[0] = len;
+	    v4[1] = '0';
+	    v4[2] = 0;
+	    v4[3] = 0;
 	}
-	return main(argc, argv);
+	return main(v2, v3);
     }
     else
     {
-	if (args[3])
+	if (v4[3])
 	{
 	    char c, t;
-	    c = (args[1] + args[2] == 3) ? 3 : (args[1] + args[2] == 4) ? 2 : 1;
-	    if (args[0] != 1) {
-		(args[0])--; t = args[2]; args[2] = c; main(argc, argv); c = args[2]; args[2] = t; (args[0])++;
+	    c = (v4[1] + v4[2] == 3) ? 3 : (v4[1] + v4[2] == 4) ? 2 : 1;
+	    if (v4[0] != 1) {
+		(v4[0])--; t = v4[2]; v4[2] = c; main(v2, v3); c = v4[2]; v4[2] = t; (v4[0])++;
 	    }
-	    printf(m1, args[0], args[1], args[2]);
-	    if (args[0] != 1) {
-		(args[0])--; t = args[1]; args[1] = c; main(argc, argv); c = args[1]; args[1] = t; (args[0])++;
+	    printf(v1, v4[0], v4[1], v4[2]);
+	    if (v4[0] != 1) {
+		(v4[0])--; t = v4[1]; v4[1] = c; main(v2, v3); c = v4[1]; v4[1] = t; (v4[0])++;
 	    }
 	}
 	else
 	{
-	    switch (- 1 + (-(args[0])) - 1)
+	    switch (- 1 + (-(v4[0])) - 1)
 	    {
 	      case 0:
-		puts(bits, bits[- len - (1 + ((args[1] & 1) ^ 1))] = args[1]);
-		puts(bits, bits[- len - (1 + (args[1] & 1))] = args[1]);
+		puts(bits, bits[- len - (1 + ((v4[1] & 1) ^ 1))] = v4[1]);
+		puts(bits, bits[- len - (1 + (v4[1] & 1))] = v4[1]);
 		break;
 	      case -1:
-		puts(bits, bits[- len - 1] = args[1]);
+		puts(bits, bits[- len - 1] = v4[1]);
 		break;
 	      default:
-		args[0] += 1 + ((args[1] & 1) ^ 1);
-		main(argc, argv);
-		args[0] -= 1 + ((args[1] & 1) ^ 1);
-		if (args[1] & 1) {
-		    (args[0])+=2; (args[1]) ^= 1; main(argc, argv); (args[1]) ^= 1; (args[0])-=2;
+		v4[0] += 1 + ((v4[1] & 1) ^ 1);
+		main(v2, v3);
+		v4[0] -= 1 + ((v4[1] & 1) ^ 1);
+		if (v4[1] & 1) {
+		    (v4[0])+=2; (v4[1]) ^= 1; main(v2, v3); (v4[1]) ^= 1; (v4[0])-=2;
 		}
-		puts(bits, bits[args[0] - len] = args[1]);
-		if (!(args[1] & 1)) {
-		    (args[0])+=2; (args[1]) ^= 1; main(argc, argv); (args[1]) ^= 1; (args[0])-=2;
+		puts(bits, bits[v4[0] - len] = v4[1]);
+		if (!(v4[1] & 1)) {
+		    (v4[0])+=2; (v4[1]) ^= 1; main(v2, v3); (v4[1]) ^= 1; (v4[0])-=2;
 		}
-		args[0] += 1 + (args[1] & 1);
-		main(argc, argv);
-		args[0] -= 1 + (args[1] & 1);
+		v4[0] += 1 + (v4[1] & 1);
+		main(v2, v3);
+		v4[0] -= 1 + (v4[1] & 1);
 	    }
 	}
     }
