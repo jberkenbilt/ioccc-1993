9c0e9d39d4a4bc489b53e194d80a7b2582b9b463
Jay Berkenbilt <ejb@ql.org>
1993-03-13 13:20:09 +0000

    Add tower of hanoi functionality
    RCS: qjbprog.c: 1.4

--- qjbprog.c
+++ qjbprog.c
@@ -1,26 +1,50 @@
 /* 
- * $Id: qjbprog.c,v 1.3 1993-03-08 04:29:27 qjb Exp $
+ * $Id: qjbprog.c,v 1.4 1993-03-13 13:20:09 qjb Exp $
  * $Source: /tmp/ioccc-1993/ioccc-1993/qjbprog.c,v $
  * $Author: qjb $
  *
- * This program sovles the "patience puzzle" also known as the Chinese
- * ring puzzle and other names.
+ * This program takes a single argument.  If the argument is positive,
+ * it sovles the case of the "patience puzzle", also known as the
+ * Chinese ring puzzle and other names, with argv[1] rings.  If the
+ * argument is negative, it solves the tower of hanoi with -argv[1]
+ * rings.
  *
  * Obfuscation ideas:  Implement the whole program as main.
- * modify argv[1]; use argv[2] as sign bit.
+ * modify argv[1]; use argv[2] as extra data.
  * have a = b == c somewhere
  * use macros to manipulate bits
+ * use arrays of numeric characters for strings
  */
 
 #if !defined(lint) && !defined(CODECENTER) || defined(RCS_HDRS)
-static char *rcsid = "$Id: qjbprog.c,v 1.3 1993-03-08 04:29:27 qjb Exp $";
+static char *rcsid = "$Id: qjbprog.c,v 1.4 1993-03-13 13:20:09 qjb Exp $";
 #endif /* !lint && !CODECENTER || RCS_HDRS */
+
+extern void *malloc(int);
 
 static char *bits;
 static int len;
 
 void add(int);
 void rem(int);
+
+move_ring(int n, int a, int b)
+{
+    printf("move ring %d from stack %d to stack %d\n", n, a, b);
+}
+
+void move_stack(int n, int a, int b, int c)
+{
+    if (n == 1)
+	move_ring(n, a, b);
+    else
+    {
+	move_stack(n - 1, a, c, b);
+	move_ring(n, a, b);
+	move_stack(n - 1, c, b, a);
+    }
+}
+
 
 void p_add(int n)
 {
@@ -89,10 +113,12 @@ int main(int argc, char *argv[])
 	puts(bits);
 	rem(len);
     }
-    else
+    else if (len < 0)
     {
-	printf("invalid\n");
+	move_stack(-len, 1, 2, 3);
     }
+    else
+	return 1, printf("invalid\n");
 
     return 0;
 }
